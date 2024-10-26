0c434f4080b6a1f37a37e44e48945dbd7ca850c7
Syeda9510 <shabananaz281@gmail.com>
2024-10-26 05:46:51 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_tMe9fb0TH', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6ufj9mtmgne8g2i16i6es0010u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
