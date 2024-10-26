eff9bec1e73d99b29f7cf1e3f528d3d2d1d9deca
Syeda9510 <shabananaz281@gmail.com>
2024-10-26 05:41:25 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_tMe9fb0TH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6ufj9mtmgne8g2i16i6es0010u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
